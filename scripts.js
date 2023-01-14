2681b87dc019ba3e2e885c0eb64954498f23a9da
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-01-13 22:23:30 -0500

    Add fuctions for basic math Added functions to add, subtract, multiply, and divide

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
+//Adds two numbers
 function add (num1, num2) {
     total = num1 + num2;
     return total;
 }
+
+//Subtracts two numbers
+function subtract (num1, num2) {
+    total = num1 - num2;
+    return total;
+}
+
+//Multiplies two numbers
+function multiply (num1, num2) {
+    total = num1 * num2;
+    return total;
+}
+//Divides two numbers
+function divide (num1, num2) {
+    total = num1 / num2;
+    return total;
+}
+
