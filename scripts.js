bd7aabb8b5e101731ff8695c704f5a7f328f6f36
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-01-16 22:08:13 -0500

    Fix display to update based on number input.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,55 @@
-let displayNumber = 0
+let initNumber = 0
+let displayNumber = ''
 
+numberOne = document.getElementById('1');
+numberOne.addEventListener('click', () => {
+    updateDisplay(1);
+});
+
+numberOne = document.getElementById('2');
+numberOne.addEventListener('click', () => {
+    updateDisplay(2);
+});
+
+numberOne = document.getElementById('3');
+numberOne.addEventListener('click', () => {
+    updateDisplay(3);
+});
+
+numberOne = document.getElementById('4');
+numberOne.addEventListener('click', () => {
+    updateDisplay(4);
+});
+
+numberOne = document.getElementById('5');
+numberOne.addEventListener('click', () => {
+    updateDisplay(5);
+});
+
+numberOne = document.getElementById('6');
+numberOne.addEventListener('click', () => {
+    updateDisplay(6);
+});
+
+numberOne = document.getElementById('7');
+numberOne.addEventListener('click', () => {
+    updateDisplay(7);
+});
+
+numberOne = document.getElementById('8');
+numberOne.addEventListener('click', () => {
+    updateDisplay(8);
+});
+
+numberOne = document.getElementById('9');
+numberOne.addEventListener('click', () => {
+    updateDisplay(9);
+});
+
+numberOne = document.getElementById('0');
+numberOne.addEventListener('click', () => {
+    updateDisplay(0);
+});
 
 //Adds two numbers
 function add (num1, num2) {
@@ -33,13 +83,27 @@ function operate (num1, num2, sign) {
     } else if (sign  == "*") {
         return multiply(num1, num2);
     } else if (sign == "/") {
-        return divide(num1, num2)
+        return divide(num1, num2);
     } else {
-        console.log("no input")
+        console.log("no input");
     }
 }
 
-const display = document.createElement('div');
+//Adds the diplay for user input
+let display = document.createElement('div');
 display.setAttribute("class", "display");
+display.setAttribute('id', 'display');
 calculator.insertBefore(display, row1);
-display.textContent = displayNumber;
+display.textContent = initNumber;
+
+//updates display as numbers are clicked
+function updateDisplay (num){
+    displayNumber = displayNumber.toString() + num;
+    remove = document.getElementById('display');
+    calculator.removeChild(remove);
+    display = document.createElement('div');
+    display.setAttribute("class", "display");
+    display.setAttribute('id', 'display');
+    calculator.insertBefore(display, row1);
+    display.textContent = displayNumber;
+}
